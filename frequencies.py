2493913d5741ed0de37559dcde75a61778dfc050
Hassan Mahmood <hassanmahmood@live.co.uk>
2023-09-26 22:25:15 +0100

    implemented frequency counter

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,12 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+    frequencies = dict()
+    for item in items:
+        item = str(item)
+        if item in frequencies:
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
+
     return frequencies
